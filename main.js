d09bc85739664a717a638da18931402e1333e57d
elvece <lvcifferello@gmail.com>
2015-07-18 09:08:33 -0600

    added pseudo coding for unsolved problems

--- main.js
+++ main.js
@@ -44,8 +44,28 @@ console.log(longestWord("This is a test sentence."));
 
 
 //Problem 3: Refactor the `longestWord` function so that it ignores punctuation.
-
+/*pseudo code:
+after spliiting, test for anything other than capital and lowercase letters
+ignore anything other than capital or lowercase letters
+also, what to do if there are two words with the same longest length? -->if else statement to output second one
+*/
 
 //Problem 4: Define a function called `factorial` that takes a random number as an argument and then returns the factorial of that given number.
+/* funcion (randomNumberInput){
+}
+make sure input is a number
+math: factorial is num*(num-1)
+if, else?
+*/
 
 //Bonus: Write a function called `palindrome` that takes a string as an argument and returns the string in reversed order. Return true if the string is the same as the reversed string, otherwise return false.
+/*function(originalString){
+}
+copy string into a new array
+split string into seperate characters
+reverse the order of the characters in the array
+maybe do this by using a loop and taking the first letter and placing it last? see if reverse function
+if reverseString is same as originalString, return true, else, return fasle
+then merge split characters back into a string
+return final string
+*/
